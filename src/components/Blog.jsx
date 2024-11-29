fc92ddc4076256bb1464035cfb340918009ffd0b
Guillermo Cerviño Porto <144793938+GuillermoGCP@users.noreply.github.com>
2024-11-29 18:51:40 +0100

    Exercise 5.6 was already done. Complete exercise 5.7 and add some css styles

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,7 +1,36 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import React from 'react'
+import './Blog.css'
+
+const Blog = ({ blog }) => {
+  const [areDetailsVisible, setAreDetailsVisible] = React.useState(true)
+
+  const toggleDetails = () => {
+    setAreDetailsVisible(!areDetailsVisible)
+  }
+
+  return areDetailsVisible ? (
+    <div className='blog-container'>
+      <div className='blog-header'>
+        <span className='blog-title'>{blog.title}</span>
+        <button className='toggle-btn' onClick={toggleDetails}>
+          Hide
+        </button>
+      </div>
+      <p className='blog-url'>{blog.url}</p>
+      <div className='blog-likes'>
+        <span>{blog.likes} likes</span>
+        <button className='like-btn'>Like</button>
+      </div>
+      <p className='blog-author'>Author: {blog.author}</p>
+    </div>
+  ) : (
+    <div className='blog-hidden'>
+      <span>{blog.title}</span>
+      <button className='toggle-btn' onClick={toggleDetails}>
+        View
+      </button>
+    </div>
+  )
+}
 
 export default Blog
