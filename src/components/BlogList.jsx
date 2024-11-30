63bf433bb44d8ece4c073ebafefe55ac9c52e10d
Guillermo Cerviño Porto <144793938+GuillermoGCP@users.noreply.github.com>
2024-11-30 19:37:38 +0100

    Exercise 5.9 was already done, and complete exercise 5.10

--- src/components/BlogList.jsx
+++ src/components/BlogList.jsx
@@ -3,9 +3,11 @@ import Blog from './Blog'
 const BlogList = ({ blogs, setBlogs }) => (
   <div>
     <h2>blogs</h2>
-    {blogs?.map((blog) => (
-      <Blog key={blog.id} blog={blog} setBlogs={setBlogs} />
-    ))}
+    {blogs
+      ?.sort((a, b) => b.likes - a.likes)
+      .map((blog) => (
+        <Blog key={blog.id} blog={blog} setBlogs={setBlogs} />
+      ))}
   </div>
 )
 
