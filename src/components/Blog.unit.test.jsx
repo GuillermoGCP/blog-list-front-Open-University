1f92d281b583521b9a15c4369411471c91c00e8e
Guillermo Cerviño Porto <144793938+GuillermoGCP@users.noreply.github.com>
2024-12-05 12:51:02 +0100

    Exercise 5.14

--- src/components/Blog.unit.test.jsx
+++ src/components/Blog.unit.test.jsx
@@ -1,5 +1,7 @@
 import { render, screen } from '@testing-library/react'
 import Blog from './Blog'
+import { beforeEach, describe, expect } from 'vitest'
+import userEvent from '@testing-library/user-event'
 
 const blogMock = {
   id: '5a422bc61b54a676234d17fc',
@@ -10,24 +12,55 @@ const blogMock = {
   user: { id: 'user1', username: 'testuser' },
 }
 
-test('renders blog title but not url, author, or likes initially', () => {
-  render(
-    <Blog
-      blog={blogMock}
-      setBlogs={() => {}}
-      setError={() => {}}
-      user={{ username: 'other-testuser' }}
-    />
-  )
-  // If the element is not found, an error is thrown.
-  expect(screen.getByText('Type wars')).toBeInTheDocument()
-
-  // If the element is not found, returns null.
-  expect(
-    screen.queryByText(
-      'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html'
+describe('render blog content', () => {
+  beforeEach(() => {
+    const mockSetBlogs = vi.fn()
+    const mockSetError = vi.fn()
+    render(
+      <Blog
+        blog={blogMock}
+        setBlogs={mockSetBlogs}
+        setError={mockSetError}
+        user={{ username: 'testuser' }}
+      />
     )
-  ).not.toBeInTheDocument()
-  expect(screen.queryByText('Robert C. Martin')).not.toBeInTheDocument()
-  expect(screen.queryByText('2 likes')).not.toBeInTheDocument()
+  })
+
+  test('renders blog title but not url, author, or likes initially', () => {
+    // If the element is not found, an error is thrown.
+    expect(screen.getByText('Type wars')).toBeInTheDocument()
+
+    // If the element is not found, returns null.
+    expect(
+      screen.queryByText(
+        'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html'
+      )
+    ).not.toBeInTheDocument()
+    expect(screen.queryByText('Robert C. Martin')).not.toBeInTheDocument()
+    expect(screen.queryByText('2 likes')).not.toBeInTheDocument()
+  })
+
+  test('Clicking the button displays all the content of the blog: title, author, URL and likes.', async () => {
+    const user = userEvent.setup()
+    const showButton = screen.getByText('View')
+
+    await user.click(showButton)
+
+    expect(screen.getByText('2 likes')).toBeInTheDocument()
+    expect(
+      screen.getByText(
+        'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html'
+      )
+    ).toBeInTheDocument()
+
+    const hideButton = screen.getByText('Hide')
+    await user.click(hideButton)
+
+    expect(screen.queryByText('2 likes')).not.toBeInTheDocument()
+    expect(
+      screen.queryByText(
+        'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html'
+      )
+    ).not.toBeInTheDocument()
+  })
 })
