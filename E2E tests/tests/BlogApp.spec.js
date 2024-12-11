8459159f2354eafaa13bc836666b323bd9100b02
Guillermo Cerviño Porto <144793938+GuillermoGCP@users.noreply.github.com>
2024-12-11 13:04:11 +0100

    Exercise 5.19

--- E2E tests/tests/BlogApp.spec.js
+++ E2E tests/tests/BlogApp.spec.js
@@ -1,5 +1,11 @@
 const { test, expect, beforeEach, describe } = require('@playwright/test')
-const { resetDb, loginFormFn, loginWith } = require('./helpers.js')
+const {
+  resetDb,
+  loginFormFn,
+  loginWith,
+  createForm,
+  blogFormFn,
+} = require('./helpers.js')
 
 let loginForm
 describe('Login', () => {
@@ -23,5 +29,59 @@ describe('Login', () => {
   test('fails with wrong credentials', async ({ page }) => {
     await loginWith(loginForm, 'ElB', 'wrongPassword')
     await expect(page.getByText('invalid username or password')).toBeVisible()
+  })
+
+  test('add new blog and cancel buttons works correctly', async ({ page }) => {
+    await loginWith(loginForm, 'ElB', 'bua234')
+
+    const showBlogFormButton = page.getByText('Add a new blog')
+    await expect(showBlogFormButton).toBeVisible()
+
+    await showBlogFormButton.click()
+    await expect(page.getByText('Author')).toBeVisible()
+    await expect(page.getByText('Title')).toBeVisible()
+    await expect(page.getByText('Url')).toBeVisible()
+
+    const cancelButton = page.getByText('cancel')
+    await cancelButton.click()
+    await expect(page.getByText('Author')).not.toBeVisible()
+    await expect(page.getByText('Title')).not.toBeVisible()
+    await expect(page.getByText('Url')).not.toBeVisible()
+  })
+
+  test('loggued user can create a new blog and it is visible', async ({
+    page,
+  }) => {
+    await loginWith(loginForm, 'ElB', 'bua234')
+    // console.log(await page.content())
+
+    const showBlogFormButton = page.getByText('Add a new blog')
+    await showBlogFormButton.click()
+
+    const blogForm = blogFormFn(page)
+    await createForm(blogForm, 'Test author', 'Test title', 'Test url')
+
+    const newBlog = page.getByText('Test title', { exact: true })
+    await expect(newBlog).toBeVisible()
+
+    const viewButton = page.locator('.toggle-btn')
+    await expect(viewButton).toBeVisible()
+    await viewButton.click()
+
+    const blogTitle = page.getByText('Test title', { exact: true })
+    const blogUrl = page.getByText('Test url', { exact: true })
+    const blogAuthor = page.getByText('Author: Test author', { exact: true })
+
+    await expect(blogTitle).toBeVisible()
+    await expect(blogUrl).toBeVisible()
+    await expect(blogAuthor).toBeVisible()
+
+    const hideButton = page.getByText('Hide')
+    await expect(viewButton).toBeVisible()
+    await hideButton.click()
+
+    await expect(blogTitle).toBeVisible()
+    await expect(blogUrl).not.toBeVisible()
+    await expect(blogAuthor).not.toBeVisible()
   })
 })
--- E2E tests/tests/helpers.js
+++ E2E tests/tests/helpers.js
@@ -17,10 +17,25 @@ const resetDb = async (request) => {
 
 const loginFormFn = (page) => {
   const loginForm = {}
-  loginForm['usernameInput'] = page.getByLabel('Username')
-  loginForm['passwordInput'] = page.getByLabel('Password')
+  loginForm['usernameInput'] = page.getByText('Username')
+  loginForm['passwordInput'] = page.getByText('Password')
   loginForm['button'] = page.getByText('Login')
   return loginForm
 }
+const blogFormFn = (page) => {
+  const blogForm = {}
+  blogForm['authorInput'] = page.getByRole('textbox', { name: 'Author' })
+  blogForm['titleInput'] = page.getByRole('textbox', { name: 'Title' })
+  blogForm['urlInput'] = page.getByRole('textbox', { name: 'Url' })
+  blogForm['button'] = page.getByText('Create')
+  return blogForm
+}
 
-export { loginWith, resetDb, loginFormFn }
+const createForm = async (blogForm, author, title, url) => {
+  await blogForm.authorInput.fill(author)
+  await blogForm.titleInput.fill(title)
+  await blogForm.urlInput.fill(url)
+  await blogForm.button.click()
+}
+
+export { loginWith, resetDb, loginFormFn, createForm, blogFormFn }
